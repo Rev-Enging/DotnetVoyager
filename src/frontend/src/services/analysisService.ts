ff8b38ff7585173c91833b83bfec0e00a7f6f153
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-11-25 19:59:29 +0200

    Add base frontend

---
+++ src/frontend/src/services/analysisService.ts
@@ -0,0 +1,23 @@
+﻿import { axiosInstance } from '../api/axios';
+import { API_ROUTES } from '../const/api-routes';
+
+export const analysisService = {
+    uploadAssembly: async (file: File) => {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        return await axiosInstance.post(API_ROUTES.ANALYSIS.UPLOAD, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+    },
+
+    getStatus: async (analysisId: string) => {
+        return await axiosInstance.get(API_ROUTES.ANALYSIS.GET_STATUS(analysisId));
+    },
+
+    getAssemblyTree: async (analysisId: string) => {
+        return await axiosInstance.get(API_ROUTES.ANALYSIS.GET_TREE(analysisId));
+    }
+};
