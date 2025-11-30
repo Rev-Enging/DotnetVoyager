99cf3cb5d2c8c4b96f0bccdfb431a13df0c8a898
Nadin <nada2205nada@gmail.com>
2025-11-30 19:09:50 +0200

    change main files

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,37 +1,20 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'*/
-import './App.css'
-import {AnalysisTester} from "./components/AnalysisTester.tsx";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { UploadPage } from './pages/UploadPage/UploadPage';
+import { AnalysisDashboard } from './pages/AnalysisDashboard/AnalysisDashboard';
+import { About } from "./pages/About/About.tsx";
+import './App.css';
 
 function App() {
-  //const [count, setCount] = useState(0)
-
-  return (
-    <>
-{/*      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>*/}
-        <AnalysisTester />
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<UploadPage />} />
+                <Route path="/analysis/:id" element={<AnalysisDashboard />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
